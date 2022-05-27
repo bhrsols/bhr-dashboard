69e23ea1432babc32aa0fd46fdd2c254d62260e5
Younes Alturkey <hi@younes.ninja>
2022-05-28 00:24:51 +0300

    fix: bugs

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -35,7 +35,7 @@ export function AppLayout(props: any) {
 
         // Disable app loading screen after 1s
         if (initializing) {
-            setTimeout(() => dispatch(setInitializing(false)), 1000)
+            setTimeout(() => dispatch(setInitializing(false)), 1500)
         }
     }, [])
 
